00c5ac2b189c09ec68c4a5258aa310b3c8df241a
Moksh Tehlan <moksh@grappus.com>
2024-07-26 21:51:53 +0530

    feat(detail-screen): deleted data screen state

--- composeApp/src/commonMain/kotlin/presentation/detail/viewmodel/DataScreenState.kt
+++
@@ -1,9 +0,0 @@
-package presentation.detail.viewmodel
-
-import domain.model.MovieDetails
-
-data class DataScreenState(
-    val isLoading: Boolean = false,
-    val error: String? = null,
-    val movieDetail: MovieDetails? = null
-)
